66c36ff72a3a12ecf272d5c2c3721f654a1f0165
PoppyCollis <poppy.collis@gmail.com>
2024-05-06 21:50:59 +0100

    integrate with adj extraction

--- observer_transition_model.py
+++ observer_transition_model.py
@@ -22,6 +22,8 @@ from scipy.stats import entropy
 import random
 import networkx as nx
 
+from algebra import * 
+
 def construct_graph():
 
     # Create a directed graph that will mimic an undirected graph by adding both directional edges
@@ -54,6 +56,26 @@ def construct_graph():
     
     return Adj
 
+def draw_graph(adj):
+    
+    # Create a directed graph
+    G = nx.DiGraph()
+
+    # Add edges based on adjacency matrix
+    num_nodes = adj.shape[0]
+    for i in range(num_nodes):
+        for j in range(num_nodes):
+            if adj[i][j] == 1:
+                G.add_edge(i, j)
+
+    # Draw the graph
+    pos = nx.spring_layout(G)  # positions for all nodes
+    nx.draw(G, pos, with_labels=True, node_color='skyblue', node_size=700, edge_color='k', linewidths=1, font_size=15)
+
+    # Show plot
+    plt.show()
+    
+
 def adj_to_transition(Adj):
     
     # convert adjacency matrix to transition matrix
@@ -83,19 +105,23 @@ def create_B(adj, mode_action_names, num_states):
     """ Take each row and extend for a discrete action """
     
     B = utils.initialize_empty_B(num_states, num_states)
-    T = adj_to_transition(adj) 
+    T = adj_to_transition(adj) # without control cost #is as expected w/ ryan adj
+    # T = control_cost_prior(adj) # control cost prior
     
     for i in range(T.shape[1]): # for each row 
-        B[0][:, :, i][i] = T[i] * 1/T[i]  # any available paths = T
+        B[0][:, :, i][i] = T[i] * 1/T[i] # without control cost 
+        # B[0][:, :, i][i] = T[i] # control cost prior
         B[0][np.isnan(B[0])] = 0
         
-    # identify any columns of zeros in each slice
+    # identify any columns of zeros in each slice 
+    # .........not necessarily col of zeros but col that doesnt sum to 1
     # get all of the neighbouring nodes to that 0 column node
     # and set the transition probs as uniformly going to neighbouring nodes instead
     for i in range(T.shape[1]):
         for j in range(T.shape[0]): # T.shape[0] should be len of columns
             if np.array_equal(B[0][:,j,i], np.zeros(T.shape[1])):
-                B[0][:,j,i] = adj[:,j]/np.sum(adj[:,j], axis=0)
+                B[0][:,j,i] = adj[:,j]/np.sum(adj[:,j], axis=0) # without control cost 
+                # B[0][:,j,i] = T[:,j]/np.sum(T[:,j], axis=0) # for cc stuff
     
     return B
         
@@ -111,11 +137,42 @@ def create_C(num_obs, rew_idx, pun=-5.,reward=5):
     C[0][rew_idx] = reward
     return C[0]
 
+def control_cost_prior(adj, control_cost=None):
+    '''
+    returns some control cost matrix as unnorm probs
+    (when the control cost is close to 0, it is less likely)
+    
+    # Also I think I should be adding this at G not in transition likelihoods...
+    # Also should av control cost of self loop be centre to border?
+    '''
+    
+    if control_cost == None:
+        # for now just have random control costs...
+        random_costs = np.random.rand(3,3) * 1000
+        
+        costs_norm = random_costs/np.sum(random_costs, axis = 1)
+        
+        costs = 1-costs_norm
+        
+        cc = adj * costs
+        
+        # set all diagonals to 1s as these will have no cost
+        cc = cc * (1-np.eye(cc.shape[0])) + np.eye(cc.shape[0])
+
+    else:
+        return None
+    return cc
+
 
 # %% CONSTRUCT AGENT
 
 # create transition model
-adj = construct_graph() # create adjacency matrix
+# adj = construct_graph() # create adjacency matrix
+
+W = np.random.randint(-10, 10, (3, 2))
+b = np.random.randint(-10, 10, (3, 1))
+adj = extract_adjacency(W, b)
+draw_graph(adj)
 
 # state
 state_modes = [f'{i}' for i in range(adj.shape[0])]
@@ -136,17 +193,18 @@ A = create_A(num_obs, num_states, state_modes, obs_modes)
 
 B = create_B(adj, mode_action_names, num_states)
 
+temp = B[0]
+
 # create prior preferences
 rew_idx = 2 # index of the rewarding observation
 C = create_C(num_obs, rew_idx, pun=-5,reward=5) 
 
 
 # construct active inference agent
-#my_agent = Agent(A=A, B=B, C=C, use_param_info_gain = False, action_selection='deterministic', alpha=16.0)
 my_agent = Agent(A=A, B=B, C=C, policy_len=4, policies=None, B_factor_list=None, action_selection="deterministic")
 
 
-# In[] Run AIF loop
+# In[] RUN ACTIVE INFERENCE LOOP
 
 my_agent.reset() # resets qs and q_pi to uniform
     
