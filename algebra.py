7919fcaf796b596280c7a4a704c4d539642c7479
Ryan <rs773@sussex.ac.uk>
2024-05-07 20:25:21 +0100

    clearer example

--- algebra.py
+++ algebra.py
@@ -17,9 +17,10 @@ def extract_adjacency(W: np.ndarray, b: np.ndarray) -> np.ndarray:
     # Find redundant inequalities by checking lp
     adjacency = []
     for i in range(len(W)):
-        logger.info(f"Checking node {str(i)}..")
+        logger.info(f"Checking node i={str(i)}..")
         W_, b_ = get_polytope_rep(W, b, i)
         redundants = check_for_redundancy(W_, b_)
+        redundants.insert(i, False)
         adjacency.append([int(not redundant) for redundant in redundants])
     return np.array(adjacency)
 
@@ -34,6 +35,8 @@ def get_polytope_rep(W: np.ndarray, b: np.ndarray, i):
     """
     w_mode = W[i]
     b_mode = b[i]
+    W = np.delete(W, (i), axis=0)
+    b = np.delete(b, (i), axis=0)
     W_ = W - w_mode
     b_ = b_mode - b
     return W_, b_
@@ -42,16 +45,16 @@ def get_polytope_rep(W: np.ndarray, b: np.ndarray, i):
 def check_for_redundancy(W, b):
     redundant = []
     for i in range(len(W)):
-        res = linprog(W[i], A_ub=W, b_ub=b)
+        res = linprog(W[i], A_ub=W, b_ub=-b)
         if res["status"] == 0:
             is_redundant = (np.abs(res["fun"] - b[i]).sum() > 0.1)
-            logger.info(f"..linear program constraint check found {str(i)}, redundant {is_redundant}")
+            logger.info(f"..linear program constraint check found j={str(i)}, redundant {is_redundant}")
             redundant.append(is_redundant)
         elif res["status"] == 3:
-            logger.info(f"..linear program constraint check is unbounded for {str(i)}")
+            logger.info(f"..linear program constraint check is unbounded for j={str(i)}")
             redundant.append(False)
         else:
-            logger.info(f"..Linear program constraint check failed for node {str(i)}")
+            logger.info(f"..Linear program constraint check failed for node j={str(i)}")
             redundant.append(True)
     return redundant
 
@@ -92,14 +95,17 @@ if __name__ == "__main__":
 
     import matplotlib.pyplot as plt
     from examples.utils import plot_phases
-    from examples.library import get_three_region_env
+    from examples.library import get_three_region_env, get_linearly_seperated_env
 
-    env = get_three_region_env(0, 0, 5)
+    env = get_three_region_env(0, 0, -5)
     
+    # env = get_linearly_seperated_env()
+
     Ws = np.block([[linear.w] for linear in env.linear_systems])
     bs = np.block([linear.b for linear in env.linear_systems])
 
-
+    print(Ws)
+    print("b", bs)
 
     # print(bs)
     # # W = np.random.randint(-10, 10, (10, 3))
@@ -108,6 +114,7 @@ if __name__ == "__main__":
     A = extract_adjacency(Ws, bs)
     print(A)
 
+    # raise
     # plot_phases(Ws, bs)
     # plt.show()
 
@@ -117,7 +124,6 @@ if __name__ == "__main__":
     from matplotlib import cm
     from matplotlib.ticker import LinearLocator
 
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
     # Make data.
     X = np.arange(-10, 10, 0.25)
@@ -126,21 +132,25 @@ if __name__ == "__main__":
     print(X[0][0])
     # Z = np.sin(R)
 
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     # Plot the surface.
     Z1 = Ws[0][0] * X  + Ws[0][1] * Y + bs[0]
-    # surf = ax.plot_surface(X, Y, Z1, cmap=cm.coolwarm,
-    #                     linewidth=0, antialiased=False)
+    surf = ax.plot_surface(X, Y, Z1, cmap=cm.coolwarm,
+                        linewidth=0, antialiased=False)
 
     Z2 = Ws[1][0] * X  + Ws[1][1] * Y +  bs[1]
-    # surf = ax.plot_surface(X, Y, Z2, cmap=cm.coolwarm,
-    #                         linewidth=0, antialiased=False)
+    surf = ax.plot_surface(X, Y, Z2, cmap=cm.coolwarm,
+                            linewidth=0, antialiased=False)
     
     Z3 = Ws[2][0] * X  + Ws[2][1] * Y +  bs[2]
-    # surf = ax.plot_surface(X, Y, Z3, cmap=cm.coolwarm,
-    #                             linewidth=0, antialiased=False)
+    surf = ax.plot_surface(X, Y, Z3, cmap=cm.coolwarm,
+                                linewidth=0, antialiased=False)
     
+
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
     all = np.stack([Z1, Z2, Z3])
-    # print(all.shape)
     Z = np.max(all, axis=0)
     # print(Z.shape)
     surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
