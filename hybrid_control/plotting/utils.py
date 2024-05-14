6d84b178286a18fbcbc1cd7cbd19551287f3b56a
Ryan <rs773@sussex.ac.uk>
2024-05-14 22:46:42 +0100

    arrows wrong direction

--- hybrid_control/plotting/utils.py
+++ hybrid_control/plotting/utils.py
@@ -17,49 +17,13 @@ ALPHA = .8
 X_LIM = (-2 , 2)
 
 
-# def plot_trajectory(z, x, ax=None, ls="-"):
-#     zcps = np.concatenate(([0], np.where(np.diff(z))[0] + 1, [z.size]))
-#     if ax is None:
-#         fig = plt.figure(figsize=(4, 4))
-#         ax = fig.gca()
-#     for start, stop in zip(zcps[:-1], zcps[1:]):
-#         ax.plot(
-#             x[start : stop + 1, 0],
-#             x[start : stop + 1, 1],
-#             lw=1,
-#             ls=ls,
-#             color=colors[z[start] % len(colors)],
-#             alpha=1.0,
-#         )
-
-#     return ax
-
-
-# def plot_original(x, ax=None, ls="-"):
-#     if ax is None:
-#         fig = plt.figure(figsize=(4, 4))
-#         ax = fig.gca()
-#     ax.plot(
-#         x[:, 0],
-#         x[:, 1],
-#         lw=1,
-#         ls=ls,
-#         # color=colors[z[start] % len(colors)],
-#         alpha=1.0,
-#     )
-
-#     return ax
-
-
 def plot_most_likely_dynamics(
     controller: Controller,
     xlim=X_LIM,
     ylim=X_LIM,
     nxpts=30,
     nypts=30,
-    alpha=ALPHA,
     ax=None,
-    figsize=(6, 6),
 ):
     K = controller.n_modes
     D = controller.obs_dim
@@ -97,7 +61,7 @@ def plot_most_likely_dynamics(
                 dxydt_m[zk, 0],
                 dxydt_m[zk, 1],
                 color=colors[k % len(colors)],
-                alpha=alpha,
+                alpha=ALPHA,
             )
             ax.legend([str(i) for i in range(K)])
             ax.set_xlim(X_LIM)
@@ -125,10 +89,10 @@ def plot_actions(controller: Controller, obs: np.ndarray, actions: np.ndarray, a
         zk = z == k
         if zk.sum(0) > 0:
             ax.quiver(
-                obs[zk, 0],
-                obs[zk, 1],
                 Bu[zk, 0],
                 Bu[zk, 1],
+                obs[zk, 0],
+                obs[zk, 1],
                 color=colors[k % len(colors)],
                 alpha=alpha,
             )
@@ -143,7 +107,7 @@ def plot_trajectory(
         controller,
         x,
         actions,
-        ls="-",
+        ls="dashed",
         ax=None,
 ):
     probs = np.stack(
@@ -180,57 +144,6 @@ def plot_suite(controller: Controller, obs: np.ndarray, actions:np.ndarray):
     plot_actions(controller, obs, actions)
     return 
 
-
-
-def _plot_most_likely_dynamics(
-    model,
-    xlim=(-4, 4),
-    ylim=(-3, 3),
-    nxpts=30,
-    nypts=30,
-    alpha=0.8,
-    ax=None,
-    figsize=(3, 3),
-):
-
-    K = model.K
-    assert model.D == 2
-    x = np.linspace(*xlim, nxpts)
-    y = np.linspace(*ylim, nypts)
-    X, Y = np.meshgrid(x, y)
-    xy = np.column_stack((X.ravel(), Y.ravel()))
-
-    # Get the probability of each state at each xy location
-    log_Ps = model.transitions.log_transition_matrices(
-        xy, np.zeros((nxpts * nypts, 0)), np.ones_like(xy, dtype=bool), None
-    )
-    z = np.argmax(log_Ps[:, 0, :], axis=-1)
-    z = np.concatenate([[z[0]], z])
-
-    if ax is None:
-        fig = plt.figure(figsize=figsize)
-        ax = fig.add_subplot(111)
-
-    for k, (A, b) in enumerate(zip(model.dynamics.As, model.dynamics.bs)):
-        dxydt_m = xy.dot(A.T) + b - xy
-
-        zk = z == k
-        if zk.sum(0) > 0:
-            ax.quiver(
-                xy[zk, 0],
-                xy[zk, 1],
-                dxydt_m[zk, 0],
-                dxydt_m[zk, 1],
-                color=colors[k % len(colors)],
-                alpha=alpha,
-            )
-
-    ax.set_xlabel("$x_1$")
-    ax.set_ylabel("$x_2$")
-
-    plt.tight_layout()
-
-    return ax
 
 
 def plot_phases(Ws, rs, ax=None, linestyle=None):
